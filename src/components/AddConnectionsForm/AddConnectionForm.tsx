db9f019b9e30428a4c9768e34bf1535904da4723
Michael Ionov <imichaelionov@gmail.com>
2023-06-24 16:13:14 +0300

    chore: more work on form

--- src/components/AddConnectionsForm/AddConnectionForm.tsx
+++ src/components/AddConnectionsForm/AddConnectionForm.tsx
@@ -1,4 +1,4 @@
-import { Button, Checkbox, Label, TextInput, Select, Dropdown } from 'flowbite-react';
+import { Alert, Button, Checkbox, Label, TextInput, Select, Dropdown } from 'flowbite-react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
@@ -15,9 +15,9 @@ const AddConnectionForm = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isDirty, isValid, isLoading, isSubmitting },
     setValue,
-    watch
+    watch,
   } = useForm<ConnectionFormInput>({
     resolver: zodResolver(ConnectionFormSchema),
     defaultValues: {
@@ -33,10 +33,20 @@ const AddConnectionForm = () => {
       params: '',
     },
   });
+
+
+  const onSubmit = (data: ConnectionFormInput) => {
+    console.log(data)
+  };
+
+  const theme = {
+    field: { input: { sizes: { sm: "text-md py-1 px-2 font-bold" } } }
+  }
+
   return (
     <div className="flex-2 p-3 max-w-lg">
       <form className="flex w-full flex-col gap-1"
-        onSubmit={handleSubmit((d: any) => console.log(d))}>
+        onSubmit={handleSubmit(onSubmit)}>
         <div>
           <h2 className="text-2xl font-bold text">{t('components.add_connection_form.title')}</h2>
         </div>
@@ -46,8 +56,7 @@ const AddConnectionForm = () => {
               <Label htmlFor="connection_name" value={t('components.add_connection_form.labels.connection_name')}
               />
             </div>
-            <TextInput {...register('connection_name')} sizing="sm" id="connection_name" minLength={2} maxLength={255} />
-            {errors?.connection_name?.message && <p className="text py-2">{errors.connection_name.message}</p>}
+            <TextInput {...register('connection_name')} sizing="sm" theme={theme} id="connection_name" minLength={2} maxLength={255} />
           </div>
           <div>
             <div className='w-full flex flex-col justify-stretch items-stretch'>
@@ -57,8 +66,8 @@ const AddConnectionForm = () => {
               <div className="flex items-center gap-3">
                 <ColorCircle {...{ color: watch('color') }} />
                 <Dropdown theme={{ "arrowIcon": "ml-2 h-4 w-4 text" }} placement="bottom" id="color" inline size="sm" className="h-60 py-2 overflow-y-auto" label="">
-                  {connectionColors.map((color) => (
-                    <Dropdown.Item id={color} onClick={() => setValue('color', color)}>
+                  {connectionColors.map((color, i) => (
+                    <Dropdown.Item key={color + i} onClick={() => setValue('color', color)}>
                       <ColorCircle {...{ color }} />
                       {titleCase(color)}
                     </Dropdown.Item>
@@ -74,15 +83,14 @@ const AddConnectionForm = () => {
               <Label htmlFor="scheme" value={t('components.add_connection_form.labels.scheme')} />
             </div>
             <Select id="scheme" required sizing="sm" >
-              {schemes.map((scheme) => (<option id={scheme}>{scheme}</option>))}
+              {schemes.map((scheme) => (<option key={scheme}>{scheme}</option>))}
             </Select>
           </div>
           <div className='w-1/4'>
             <div className="my-1 block">
               <Label htmlFor="port" value={t('components.add_connection_form.labels.port')} />
             </div>
-            <TextInput {...register('port')} type="number" sizing="sm" id="port" />
-            {errors?.port?.message && <p>{errors.port.message}</p>}
+            <TextInput {...register('port', { valueAsNumber: true, min: 1, max: 65335 })} sizing="sm" id="port" />
           </div>
         </div>
         <div className="flex gap-3">
@@ -91,14 +99,12 @@ const AddConnectionForm = () => {
               <Label htmlFor="host" value={t('components.add_connection_form.labels.host')} />
             </div>
             <TextInput {...register('host')} sizing="sm" id="host" />
-            {errors?.host?.message && <p>{errors.host.message}</p>}
           </div>
           <div className='w-1/4'>
             <div className="my-1 block">
               <Label htmlFor="dbname" value={t('components.add_connection_form.labels.dbname')} />
             </div>
             <TextInput {...register('dbname')} sizing="sm" id="dbname" />
-            {errors?.dbname?.message && <p>{errors.dbname.message}</p>}
           </div>
         </div>
 
@@ -108,24 +114,32 @@ const AddConnectionForm = () => {
               <Label htmlFor="username" value={t('components.add_connection_form.labels.username')} />
             </div>
             <TextInput {...register('username')} sizing="sm" id="username" />
-            {errors?.username?.message && <p>{errors.username.message}</p>}
           </div>
           <div className='w-full'>
             <div className="my-1 block">
               <Label htmlFor="password" value={t('components.add_connection_form.labels.password')} />
             </div>
             <TextInput {...register('password')} type="password" sizing="sm" id="password" />
-            {errors?.password?.message && <p>{errors.password.message}</p>}
           </div>
         </div>
-
         <div className="flex items-center gap-2 my-2">
           <Checkbox id="save_password" {...register('save_password')} />
           <Label htmlFor="save_password">
             {t('components.add_connection_form.labels.save_password')}
           </Label>
         </div>
-        <Button type="submit" size="sm">{t('components.add_connection_form.title')}</Button>
+        <div>
+          <Alert color="failure" className="py-2">
+            <>
+              {Object.entries(errors).map(([key, { message }]) => (
+                <p key={key} className="text-bold">
+                  {t(`components.add_connection_form.labels.${key}`)}: {message}
+                </p>
+              ))}
+            </>
+          </Alert>
+        </div>
+        <Button disabled={!isDirty || isLoading || isSubmitting} type="submit" size="sm">{t('components.add_connection_form.title')}</Button>
       </form>
     </div>
   )
