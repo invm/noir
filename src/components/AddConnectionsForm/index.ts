809ccbc9b64d38d20b3c2fd56eea42cf65bdc3f4
Michael Ionov <imichaelionov@gmail.com>
2023-06-24 16:13:14 +0300

    chore: more work on form

--- src/components/AddConnectionsForm/index.ts
+++ src/components/AddConnectionsForm/index.ts
@@ -6,17 +6,21 @@ const MAX_LENGTH_STR = 255;
 const MAX_PORT = 65535;
 const MIN_PORT = 1;
 
+const messages = {
+  length: 'Must be between 2 and 255 characters',
+}
+
 export const ConnectionFormSchema = z.object({
-  connection_name: z.string().min(MIN_LENGTH_STR).max(MAX_LENGTH_STR),
+  connection_name: z.string().min(MIN_LENGTH_STR, messages.length).max(MAX_LENGTH_STR, messages.length),
   color: z.enum(connectionColors),
   scheme: z.enum(schemes),
-  username: z.string().min(MIN_LENGTH_STR).max(MAX_LENGTH_STR),
-  password: z.string().min(MIN_LENGTH_STR).max(MAX_LENGTH_STR),
+  username: z.string().min(MIN_LENGTH_STR, messages.length).max(MAX_LENGTH_STR, messages.length),
+  password: z.string().min(MIN_LENGTH_STR, messages.length).max(MAX_LENGTH_STR, messages.length),
   save_password: z.boolean().default(false),
-  host: z.string().min(MIN_LENGTH_STR).max(MAX_LENGTH_STR),
+  host: z.string().min(MIN_LENGTH_STR, messages.length).max(MAX_LENGTH_STR, messages.length),
   port: z.number().min(MIN_PORT).max(MAX_PORT),
-  dbname: z.string().min(MIN_LENGTH_STR).max(MAX_LENGTH_STR),
-  params: z.string().min(MIN_LENGTH_STR).max(MAX_LENGTH_STR),
+  dbname: z.string().optional(),
+  params: z.string().optional(),
 });
 
 export type ConnectionFormInput = z.infer<typeof ConnectionFormSchema>;
