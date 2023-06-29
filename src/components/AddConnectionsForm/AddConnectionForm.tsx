7db619824fec25507cc77cb1579cdc2ec9678682
Michael Ionov <michael@appdome.com>
2023-06-29 16:48:44 +0300

    wip: database file input

--- src/components/AddConnectionsForm/AddConnectionForm.tsx
+++ src/components/AddConnectionsForm/AddConnectionForm.tsx
@@ -1,11 +1,12 @@
 import { Alert, Button, Checkbox, TextInput, Select } from '../UI';
 import { ConnectionFormSchema } from '.';
-import { ConnectionColor, connectionColors, PORTS_MAP, Schemes, SchemeType, schemes } from '../../interfaces';
+import { ConnectionColor, connectionColors, PORTS_MAP, Schemes, SchemeType, schemes, connectionModes, AvailableConnectionModes } from '../../interfaces';
 import { titleCase } from '../../utils/formatters';
 import { useFormHandler } from 'solid-form-handler';
 import { zodSchema } from 'solid-form-handler/zod';
 import { t } from 'i18next';
-import { onMount, Show } from 'solid-js';
+import { createEffect, Match, onMount, Show, Switch } from 'solid-js';
+import { FileInput } from '../UI/FileInput';
 
 const ColorCircle = (props: { color: ConnectionColor }) => {
   return <span class={`min-w-[20px] min-h-[20px] mb-1 rounded-full border-2 bg-${props.color}-500`}></span>
@@ -16,6 +17,7 @@ const defaultValues = {
   scheme: Schemes.MySQL,
   port: 3306,
   color: 'orange',
+  mode: AvailableConnectionModes[Schemes.MySQL][0],
   host: '',
   username: '',
   password: '',
@@ -37,6 +39,7 @@ const AddConnectionForm = () => {
   });
 
   const submit = async (event: Event) => {
+    console.log(formData())
     event.preventDefault();
     try {
       await formHandler.validateForm();
@@ -46,6 +49,10 @@ const AddConnectionForm = () => {
       console.error(error);
     }
   };
+
+  createEffect(() => {
+    console.log("scheme changed", formData().scheme)
+  })
 
   return (
     <div class="flex-2 p-3 max-w-lg">
@@ -75,10 +82,11 @@ const AddConnectionForm = () => {
             </div>
           </div>
         </div>
-        <div class="flex gap-3">
-          <div class='w-3/4'>
+        <div class="grid grid-cols-6 gap-3">
+          <div class={formData().scheme === Schemes.SQLite ? 'col-span-6' : 'col-span-2'}>
             <Select
               id="scheme"
+              name="scheme"
               label={t('components.add_connection_form.labels.scheme')}
               options={schemes.map((sc) => ({ value: sc, label: titleCase(sc) }))}
               formHandler={formHandler}
@@ -87,27 +95,52 @@ const AddConnectionForm = () => {
               }}
             />
           </div>
-          <div class='w-1/4'>
-            <TextInput
-              label={t('components.add_connection_form.labels.port')}
-              name="port" id="port" formHandler={formHandler} type="number"
-              min={1} max={65335} />
-          </div>
+          <Show when={formData().scheme !== Schemes.SQLite}>
+            <div class='col-span-2'>
+              <Select
+                id="mode"
+                name="mode"
+                label={t('components.add_connection_form.labels.mode')}
+                options={AvailableConnectionModes[formData().scheme].map((md) => ({ value: md, label: titleCase(md) }))}
+                formHandler={formHandler}
+                onChange={(e) => {
+                  setFieldValue('port', PORTS_MAP[e.target.value as SchemeType] || 3306)
+                }}
+              />
+            </div>
+          </Show>
+          <Show when={formData().scheme !== Schemes.SQLite}>
+            <div class='col-span-2'>
+              <TextInput
+                label={t('components.add_connection_form.labels.dbname')}
+                min={1} max={255}
+                name="dbname" id="dbname" formHandler={formHandler} />
+            </div>
+          </Show>
         </div>
-        <div class="flex gap-3">
-          <div class='w-3/4'>
-            <TextInput
-              label={t('components.add_connection_form.labels.host')}
-              min={1} max={255}
-              name="host" id="host" formHandler={formHandler}
-            />
+        <div class="grid grid-cols-6 gap-3">
+          <div class='col-span-4'>
+            <Switch fallback={<div>Not Found</div>}>
+              <Match when={formData().scheme === Schemes.SQLite}>
+                <FileInput label={t('components.add_connection_form.labels.file')} formHandler={formHandler} name="file" />
+              </Match>
+              <Match when={formData().scheme !== Schemes.SQLite}>
+                <TextInput
+                  label={t('components.add_connection_form.labels.host')}
+                  min={1} max={255}
+                  name="host" id="host" formHandler={formHandler}
+                />
+              </Match>
+            </Switch>
           </div>
-          <div class='w-1/4'>
-            <TextInput
-              label={t('components.add_connection_form.labels.dbname')}
-              min={1} max={255}
-              name="dbname" id="dbname" formHandler={formHandler} />
-          </div>
+          <Show when={formData().scheme !== Schemes.SQLite}>
+            <div class='col-span-2'>
+              <TextInput
+                label={t('components.add_connection_form.labels.port')}
+                name="port" id="port" formHandler={formHandler} type="number"
+                min={1} max={65335} />
+            </div>
+          </Show>
         </div>
 
         <div class="grid grid-cols-2 gap-3">
@@ -126,7 +159,7 @@ const AddConnectionForm = () => {
           </div>
         </div>
         <div class="flex items-center gap-2 my-2">
-          <Checkbox label={t('components.add_connection_form.labels.save_password')} id="save_password" formHandler={formHandler} />
+          <Checkbox name="save_password" label={t('components.add_connection_form.labels.save_password')} id="save_password" formHandler={formHandler} />
         </div>
         <Show when={Object.keys(getFormErrors()).length}>
           <Alert color="error">
@@ -137,9 +170,9 @@ const AddConnectionForm = () => {
             ))}
           </Alert>
         </Show>
-        <Button disabled={isFormInvalid || !formHasChanges} type="submit" >{t('components.add_connection_form.title')}</Button>
-      </form>
-    </div>
+        <Button type="submit" >{t('components.add_connection_form.title')}</Button>
+      </form >
+    </div >
   )
 }
 
