a9a4428c00dd1979d6322cf23ce6230f2e1b47d9
Michael Ionov <michael@appdome.com>
2023-07-03 19:00:44 +0300

    wip: start work on console

--- src/components/main/Console.tsx
+++ src/components/main/Console.tsx
@@ -1,7 +1,29 @@
+import Split from 'split.js'
+import { onMount } from "solid-js"
+
 export const Console = () => {
+  onMount(() => {
+    Split(['#sidebar', '#main'], {
+      sizes: [25, 75],
+      minSize: [300, 500],
+      maxSize: [500, 1000],
+      expandToMin: true,
+    })
+    Split(['#query', '#results'], {
+      sizes: [40, 60],
+      minSize: [100, 200],
+      direction: 'vertical',
+    })
+  })
   return (
-    <div class="bg-red-500 w-full h-full">
-      
+    <div class="w-full h-full">
+      <div class="flex w-full h-full">
+        <div id="sidebar" class="bg-red-300 h-full"></div>
+        <div id="main" class="flex flex-col bg-lime-500 h-full">
+          <div id="query" class="bg-indigo-500"></div>
+          <div id="results" class="bg-blue-500"></div>
+        </div>
+      </div>
     </div>
   )
 }
