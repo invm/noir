2e370c9489ffbdab5675859f7bed0561c2a1eeeb
Michael Ionov <imichaelionov@gmail.com>
2023-07-11 22:08:52 +0300

    fix: theme change and connection tabs

--- src/components/UI/ThemeSwitch.tsx
+++ src/components/UI/ThemeSwitch.tsx
@@ -1,20 +1,25 @@
 import { For } from "solid-js"
 import { titleCase } from "../../utils/formatters"
-const THEMES = ["light", "dark", "aqua", "synthwave", "dracula", "night", "cupcake"]
+const THEMES = ["light", "dark", "aqua", "synthwave", "dracula", "night", "cupcake"] as const;
 import { onMount } from 'solid-js'
-import { themeChange } from 'theme-change'
 
 const ThemeSwitch = () => {
-
   onMount(async () => {
-    themeChange();
+    const theme = localStorage.getItem("theme") || "dark"
+    document.documentElement.dataset.theme = theme
   })
+
+  const select = (theme: typeof THEMES[number]) => {
+    document.documentElement.dataset.theme = theme
+    localStorage.setItem("theme", theme)
+  }
+
   return (
     <details class="dropdown dropdown-left">
       <summary class="m-1 btn btn-xs">Theme</summary>
       <ul class="p-2 shadow menu dropdown-content  z-[1] bg-base-100 rounded-box w-52">
         <For each={THEMES}>
-          {(theme) => <li data-toggle-theme={theme} class="py-1">{titleCase(theme)}</li>}
+          {(theme) => <li  class="py-1"><a onClick={()=> select(theme)}>{titleCase(theme)}</a></li>}
         </For>
       </ul>
     </details>
