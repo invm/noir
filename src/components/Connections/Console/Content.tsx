6bad22e2131095fe6a5db92886eb54ad485ef23d
Michael Ionov <imichaelionov@gmail.com>
2023-07-14 22:52:56 +0300

    refactor: merge query and results into one component

---
+++ src/components/Connections/Console/Content.tsx
@@ -0,0 +1,101 @@
+import { createEffect, createSignal, For, onCleanup, onMount, Show } from "solid-js"
+import { createStore } from 'solid-js/store'
+import { Store } from "tauri-plugin-store-api";
+import { t } from "../../../utils/i18n"
+import Split from "split.js"
+import { AddIcon, CloseIcon } from "../../UI/Icons";
+import { ConnectionConfig } from "../../../interfaces";
+import { QueryTextArea } from "./QueryTextArea";
+import { ResultsArea } from "./ResultsArea";
+
+const store = new Store(".console.dat");
+
+type QueryTab = {
+  query: string
+}
+
+const Content = (props: { connection: ConnectionConfig }) => {
+  // TODO: remove from local storage when parent tab is deleted
+  const CONNECTION_STORE_STORAGE_KEY = '_connection:' + props.connection.id
+  const [activeTab, setActiveTab] = createSignal(0)
+  const [tabs, setTabs] = createStore<QueryTab[]>([])
+
+  onMount(async () => {
+    const tabStr = await store.get(CONNECTION_STORE_STORAGE_KEY)
+    if (!tabStr) {
+      setTabs(() => [{ query: '' }])
+      return;
+    }
+    setTabs(() => tabStr as QueryTab[])
+  })
+  const persistState = async () => {
+    await store.set(CONNECTION_STORE_STORAGE_KEY, tabs);
+    await store.save();
+  }
+
+  const addTab = async () => {
+    setTabs([...tabs, { query: '' }])
+    setActiveTab(tabs.length - 1)
+    await persistState()
+  }
+
+  const updateQueryText = async (query: string) => {
+    setTabs([...tabs.map((t, i) => i === activeTab() ? { ...t, query } : t)])
+    await persistState()
+  }
+
+  const removeTab = (idx: number) => {
+    if (idx <= 0) return;
+    setActiveTab(0)
+    setTabs(tabs.filter((_t, i) => i !== idx));
+  }
+
+  const closeTab = async (id: number) => {
+    if (!id) return;
+    removeTab(id)
+    setActiveTab(0)
+  }
+
+  createEffect(() => {
+    const q = Split(['#query', '#results'], {
+      sizes: [40, 60],
+      minSize: [100, 200],
+      direction: 'vertical',
+      gutterSize: 8,
+    })
+    onCleanup(() => {
+      q.destroy()
+    })
+  })
+
+  return (
+    <div class="flex flex-col h-full">
+      <div id="query">
+        <div class="bg-base-100 w-full h-full flex flex-col">
+          <div class="bg-base-300 tabs gap-1">
+            <For each={tabs}>
+              {(_tab, idx) =>
+                <div onClick={() => setActiveTab(idx())} class="tab py-2 tab-md tab-lifted"
+                  classList={{ 'tab-active': activeTab() === idx() }}
+                >{t('components.console.query')} #{idx() + 1}
+                  <Show when={idx() > 0}>
+                    <button onClick={() => closeTab(idx())} class="pl-2 mb-1">
+                      <CloseIcon />
+                    </button>
+                  </Show>
+                </div>
+              }
+            </For>
+            <div onClick={() => addTab()} class="tab py-2 tab-sm tab-lifted tab-active" ><AddIcon /></div>
+          </div>
+          <QueryTextArea query={tabs[activeTab()]?.query} {...{ updateQueryText }} />
+        </div>
+      </div>
+      <div id="results" class="bg-base-200 p-3">
+        <ResultsArea />
+      </div>
+    </div>
+  )
+}
+
+export default Content
--- src/components/main/Console.tsx
+++ src/components/main/Console.tsx
@@ -1,35 +1,10 @@
 import Split from 'split.js'
-import { createEffect, createSignal, For, onCleanup, onMount, Show } from "solid-js"
+import { createEffect, onCleanup } from "solid-js"
 import { ConnectionConfig, DbSchema } from '../../interfaces'
 import { Sidebar } from '../Connections/Console/Sidebar'
-import { QueryTextArea } from '../Connections/Console/QueryTextArea'
-import { ResultsArea } from '../Connections/Console/ResultsArea'
-import { AddIcon, CloseIcon } from '../UI/Icons'
-import { createStore } from 'solid-js/store'
-import { t } from '../../utils/i18n'
-import { Store } from "tauri-plugin-store-api";
-
-const store = new Store(".console.dat");
-
-type QueryTab = {
-  query: string
-}
+import Content from '../Connections/Console/Content'
 
 export const Console = (props: { schema: DbSchema, connection: ConnectionConfig }) => {
-  // TODO: remove from local storage when parent tab is deleted
-  const CONNECTION_STORE_STORAGE_KEY = '_connection:' + props.connection.id
-  const [activeTab, setActiveTab] = createSignal(0)
-  const [tabs, setTabs] = createStore<QueryTab[]>([])
-
-  onMount(async () => {
-    const tabStr = await store.get(CONNECTION_STORE_STORAGE_KEY)
-    if (!tabStr) {
-      setTabs(() => [{ query: '' }])
-      return;
-    }
-    setTabs(() => tabStr as QueryTab[])
-  })
-
   createEffect(() => {
     const s = Split(['#sidebar', '#main'], {
       sizes: [20, 80],
@@ -37,45 +12,10 @@ export const Console = (props: { schema: DbSchema, connection: ConnectionConfig 
       maxSize: [400, Infinity],
       gutterSize: 12,
     })
-    const q = Split(['#query', '#results'], {
-      sizes: [40, 60],
-      minSize: [100, 200],
-      direction: 'vertical',
-      gutterSize: 8,
-    })
     onCleanup(() => {
       s.destroy()
-      q.destroy()
     })
   })
-
-  const persistState = async () => {
-    await store.set(CONNECTION_STORE_STORAGE_KEY, tabs);
-    await store.save();
-  }
-
-  const addTab = async () => {
-    setTabs([...tabs, { query: '' }])
-    setActiveTab(tabs.length - 1)
-    await persistState()
-  }
-
-  const updateQueryText = async (query: string) => {
-    setTabs([...tabs.map((t, i) => i === activeTab() ? { ...t, query } : t)])
-    await persistState()
-  }
-
-  const removeTab = (idx: number) => {
-    if (idx <= 0) return;
-    setActiveTab(0)
-    setTabs(tabs.filter((_t, i) => i !== idx));
-  }
-
-  const closeTab = async (id: number) => {
-    if (!id) return;
-    removeTab(id)
-    setActiveTab(0)
-  }
 
   return (
     <div class="w-full h-full bg-base-300">
@@ -85,31 +25,8 @@ export const Console = (props: { schema: DbSchema, connection: ConnectionConfig 
             <Sidebar schema={props.schema} />
           </div>
         </div>
-        <div id="main" class="flex flex-col h-full">
-          <div id="query" class="bg-base-300 rounded-bl-lg">
-            <div class="bg-base-100 w-full h-full flex flex-col rounded-l-lg">
-              <div class="bg-base-300 tabs rounded-none gap-1">
-                <For each={tabs}>
-                  {(_tab, idx) =>
-                    <div onClick={() => setActiveTab(idx())} class="tab py-2 tab-md tab-lifted"
-                      classList={{ 'tab-active': activeTab() === idx() }}
-                    >{t('components.console.query')} #{idx() + 1}
-                      <Show when={idx() > 0}>
-                        <button onClick={() => closeTab(idx())} class="pl-2 mb-1">
-                          <CloseIcon />
-                        </button>
-                      </Show>
-                    </div>
-                  }
-                </For>
-                <div onClick={() => addTab()} class="tab py-2 tab-sm tab-lifted tab-active" ><AddIcon /></div>
-              </div>
-              <QueryTextArea query={tabs[activeTab()]?.query} {...{ updateQueryText }} />
-            </div>
-          </div>
-          <div id="results" class="bg-base-200 rounded-tl-lg p-3">
-            <ResultsArea />
-          </div>
+        <div id="main">
+          <Content connection={props.connection} />
         </div>
       </div>
     </div>
