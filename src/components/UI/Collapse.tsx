f429acf203f762944ab277241d2288b44322f830
Michael Ionov <michael@appdome.com>
2023-07-26 19:00:48 +0300

    chore: fix siderbar ui

--- src/components/UI/Collapse.tsx
+++ src/components/UI/Collapse.tsx
@@ -1,11 +1,15 @@
 import { createSignal, JSXElement } from "solid-js"
+import { ChevronRight } from "./Icons"
 
 export const Collapse = (props: { title: string, children: JSXElement }) => {
   const [open, setOpen] = createSignal(false)
   return (
     <div class="w-full">
-      <div onClick={() => setOpen(!open())} class="collapse rounded-sm text-sm font-small pointer">
-        {props.title}
+      <div onClick={() => setOpen(!open())} class="collapse flex items-center rounded-sm text-sm font-semibold pointer border-b-2 border-base-300">
+        <ChevronRight />
+        <span class="ml-2">
+          {props.title}
+        </span>
       </div>
       {open() && props.children}
     </div>
