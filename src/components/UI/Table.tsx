1ddb5ccdddf836c2064464acb8bc3f0c290a63f6
Michael Ionov <imichaelionov@gmail.com>
2023-08-08 22:54:50 +0300

    feat: solve large data rendering

--- src/components/UI/Table.tsx
+++ src/components/UI/Table.tsx
@@ -1,29 +1,28 @@
-import { createEffect, createSignal, For } from "solid-js";
-import { TabulatorFull as Tabulator } from 'tabulator-tables';
+import { createEffect } from "solid-js";
+import { TabulatorFull as Tabulator } from "tabulator-tables";
 
 export const Table = (props: { data: Record<string, any>[] }) => {
-  const [columns, setColumns] = createSignal<Record<string, any>[]>([])
-  const [tabledata, setTableData] = createSignal<Record<string, any>[]>([])
-
   createEffect(() => {
-    console.log('render')
     if (!props.data.length) return;
-    const columns = Object.keys(props.data[0]).map((k) => ({ title: k, field: k }))
-    setColumns(columns)
-    setTableData(props.data)
-    console.log(props.data, columns)
+    const columns = Object.keys(props.data[0]).map((k) => ({
+      title: k,
+      field: k,
+    }));
 
-    new Tabulator('#results-table', {
+    new Tabulator("#results-table", {
       data: props.data,
-      columns: columns,
+      columns,
       layout: "fitColumns",
-      resizableColumnFit: true,
+      autoResize: true,
+      pagination: true,
+      height: "100%",
+      paginationCounter:"rows",
     });
   });
 
   return (
-    <div class="">
+    <>
       <div id="results-table"></div>
-    </div>
-  )
-}
+    </>
+  );
+};
