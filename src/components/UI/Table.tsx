6b5b92cc664b2ced7cc6852d8a1459100dbe1e2e
Michael Ionov <michael@appdome.com>
2023-08-15 19:01:51 +0300

    fix: column resize, focus after command palette action, line wrap for editor

--- src/components/UI/Table.tsx
+++ src/components/UI/Table.tsx
@@ -7,17 +7,23 @@ export const Table = (props: { data: Record<string, any>[] }) => {
     const columns = Object.keys(props.data[0]).map((k) => ({
       title: k,
       field: k,
+      resizeable: true,
     }));
 
     new Tabulator("#results-table", {
       data: props.data,
       columns,
-      layout: "fitColumns",
+      columnDefaults: {
+        title: '',
+        width: 350,
+      },
+      layout: "fitDataStretch",
       autoResize: true,
+      clipboard:true,
       pagination: true,
-      paginationSize: 30,
+      paginationSize: 20,
       height: "100%",
-      paginationCounter:"rows",
+      paginationCounter: "rows",
     });
   });
 
