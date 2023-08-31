656e41a0e6d0a5c065ed27f06a980b9db260e853
Michael Ionov <imichaelionov@gmail.com>
2023-08-31 20:14:11 +0300

    fix: table structure

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,12 +1,14 @@
+import { SORT_ORDER, TableStructureResult } from "interfaces";
+
 export const omit = (obj: any, ...keys: string[]) => {
   const copy = { ...obj };
-  keys.forEach(key => delete copy[key]);
+  keys.forEach((key) => delete copy[key]);
   return copy;
-}
+};
 
 export const firstKey = (obj: Record<string, any>) => {
   for (const key in obj) return key;
-}
+};
 
 export const randomId = () => Math.random().toString(36).substring(12);
 
@@ -21,5 +23,7 @@ export const debounce = (func: Function, wait: number) => {
 
     clearTimeout(timer);
     timer = setTimeout(later, wait);
-  }
+  };
 };
+
+export const sortTableStructure = (tableStructure: TableStructureResult) => { };
