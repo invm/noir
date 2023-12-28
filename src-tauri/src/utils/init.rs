d84c324173f7818feface2598959508220f069a5
Michael Ionov <imichaelionov@gmail.com>
2023-12-28 22:37:30 +0200

    chore: cleanuo

--- src-tauri/src/utils/init.rs
+++ src-tauri/src/utils/init.rs
@@ -4,15 +4,13 @@ use crate::database::queries::create_app_db;
 
 use super::{
     crypto::create_app_key,
-    fs::{check_if_app_dir_exists, create_app_config, create_app_dir, get_app_path},
+    fs::{check_if_app_dir_exists, create_app_dir, get_app_path},
 };
 
 pub fn init_app() -> Result<()> {
-    // TODO: clear this shit
     let app_path = get_app_path();
     if !check_if_app_dir_exists(&app_path) {
         create_app_dir(&app_path)?;
-        create_app_config(&app_path)?;
         create_app_key()?;
         create_app_db(&app_path)?;
     }
