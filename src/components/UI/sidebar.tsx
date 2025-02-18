593d78c1b16322872eadb7dc6fe5a6eef42bf589
Michael Ionov <imichaelionov@gmail.com>
2025-02-18 18:46:06 +0200

    fix: overflow

--- src/components/UI/sidebar.tsx
+++ src/components/UI/sidebar.tsx
@@ -383,7 +383,7 @@ export const SidebarInset = (props: ComponentProps<'main'>) => {
   return (
     <main
       class={cn(
-        'bg-background relative flex min-h-svh flex-1 flex-col',
+        'bg-background relative flex min-h-svh flex-1 flex-col overflow-hidden',
         'peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow',
         local.class
       )}
